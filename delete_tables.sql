39945f147a6f53837de9fa38a545e3aa5f6ac88f
indicium15 <67154087+indicium15@users.noreply.github.com>
2023-03-17 19:43:48 +0800

    added delete_tables file

--- delete_tables.sql
+++ delete_tables.sql
@@ -1,13 +1,29 @@
-DROP TABLE dbo.books
-DROP TABLE dbo.complaints
-DROP TABLE dbo.complaints_on_books
-DROP TABLE dbo.complaints_on_orders
-DROP TABLE dbo.customers
-DROP TABLE dbo.employees
-DROP TABLE dbo.items_in_orders
-DROP TABLE dbo.magazines
-DROP TABLE dbo.orders
-DROP TABLE dbo.orderstatus
-DROP TABLE dbo.price_history
-DROP TABLE dbo.publication
-DROP TABLE dbo.stocks_in_bookstores
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[books]') AND type in (N'U'))
+DROP TABLE [dbo].[books]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[bookstore]') AND type in (N'U'))
+DROP TABLE [dbo].[bookstore]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[complaints]') AND type in (N'U'))
+DROP TABLE [dbo].[complaints]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[complaints_on_bookstore]') AND type in (N'U'))
+DROP TABLE [dbo].[complaints_on_bookstore]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[complaints_on_orders]') AND type in (N'U'))
+DROP TABLE [dbo].[complaints_on_orders]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[customers]') AND type in (N'U'))
+DROP TABLE [dbo].[customers]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[employees]') AND type in (N'U'))
+DROP TABLE [dbo].[employees]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[items_in_orders]') AND type in (N'U'))
+DROP TABLE [dbo].[items_in_orders]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[magazines]') AND type in (N'U'))
+DROP TABLE [dbo].[magazines]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[orders]') AND type in (N'U'))
+DROP TABLE [dbo].[orders]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[orderstatus]') AND type in (N'U'))
+DROP TABLE [dbo].[orderstatus]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[price_history]') AND type in (N'U'))
+DROP TABLE [dbo].[price_history]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[publication]') AND type in (N'U'))
+DROP TABLE [dbo].[publication]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[stocks_in_bookstores]') AND type in (N'U'))
+DROP TABLE [dbo].[stocks_in_bookstores]
+GO
