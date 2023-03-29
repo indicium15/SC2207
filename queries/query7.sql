42d4ece97cb740306177f446d8eded2706b8e2c0
indicium15 <67154087+indicium15@users.noreply.github.com>
2023-03-29 22:06:40 +0800

    changed query7 to uppercase

--- queries/query7.sql
+++ queries/query7.sql
@@ -1,52 +1,51 @@
-
-select T.cid as Customer, title as Title, Q.HighestPub as Price
-from items_in_orders_4 as T
-join 
-	(select distinct Z.cid as selectedCus
-	from complaints as Z
-	group by Z.cid
-	having Count(Z.cid) = 
-	(select Max(NumofCom) as MaxComplaints
-	from (select Y.cid, Count(Y.complaintsid) as NumOfCom
-	from complaints as Y
-	group by Y.cid) as X)) as S
-on T.cid = S.selectedCus
-join
-	(select T.cid, max(item_price) as HighestPub
-	from items_in_orders_1 as W Join items_in_orders_4 as T
-	on W.orderid = T.orderid
-	group by T.cid) as Q
-on Q.cid = T.cid
-join items_in_orders_1 as W
-on T.orderid = W.orderid
-join stocks_in_bookstores as V
-on W.stockid = V.stockid
-join magazines as U
-on V.pubid = U.pubid
-where Q.HighestPub = W.item_price
-union all
-select T.cid as Customer, title as Title, Q.HighestPub as Price
-from items_in_orders_4 as T
-join 
-	(select distinct Z.cid as selectedCus
-	from complaints as Z
-	group by Z.cid
-	having Count(Z.cid) = 
-	(select Max(NumofCom) as MaxComplaints
-	from (select Y.cid, Count(Y.complaintsid) as NumOfCom
-	from complaints as Y
-	group by Y.cid) as X)) as S
-on T.cid = S.selectedCus
-join
-	(select T.cid, max(item_price) as HighestPub
-	from items_in_orders_1 as W Join items_in_orders_4 as T
-	on W.orderid = T.orderid
-	group by T.cid) as Q
-on Q.cid = T.cid
-join items_in_orders_1 as W
-on T.orderid = W.orderid
-join stocks_in_bookstores as V
-on W.stockid = V.stockid
-join books as U
-on V.pubid = U.pubid
-where Q.HighestPub = W.item_price
+SELECT T.cid AS Customer, title AS Title, Q.HighestPub AS Price
+FROM items_in_orders_4 AS T
+JOIN 
+	(SELECT DISTINCT Z.cid AS SELECTedCus
+	FROM complaints AS Z
+	GROUP BY Z.cid
+	HAVING Count(Z.cid) = 
+	(SELECT Max(NumofCom) AS MaxComplaints
+	FROM (SELECT Y.cid, Count(Y.complaintsid) AS NumOfCom
+	FROM complaints AS Y
+	GROUP BY Y.cid) AS X)) AS S
+ON T.cid = S.SELECTedCus
+JOIN
+	(SELECT T.cid, max(item_price) AS HighestPub
+	FROM items_in_orders_1 AS W JOIN items_in_orders_4 AS T
+	ON W.orderid = T.orderid
+	GROUP BY T.cid) AS Q
+ON Q.cid = T.cid
+JOIN items_in_orders_1 AS W
+ON T.orderid = W.orderid
+JOIN stocks_in_bookstores AS V
+ON W.stockid = V.stockid
+JOIN magazines AS U
+ON V.pubid = U.pubid
+WHERE Q.HighestPub = W.item_price
+UNION ALL
+SELECT T.cid AS Customer, title AS Title, Q.HighestPub AS Price
+FROM items_in_orders_4 AS T
+JOIN 
+	(SELECT DISTINCT Z.cid AS SELECTedCus
+	FROM complaints AS Z
+	GROUP BY Z.cid
+	HAVING Count(Z.cid) = 
+	(SELECT Max(NumofCom) AS MaxComplaints
+	FROM (SELECT Y.cid, Count(Y.complaintsid) AS NumOfCom
+	FROM complaints AS Y
+	GROUP BY Y.cid) AS X)) AS S
+ON T.cid = S.SELECTedCus
+JOIN
+	(SELECT T.cid, max(item_price) AS HighestPub
+	FROM items_in_orders_1 AS W JOIN items_in_orders_4 AS T
+	ON W.orderid = T.orderid
+	GROUP BY T.cid) AS Q
+ON Q.cid = T.cid
+JOIN items_in_orders_1 AS W
+ON T.orderid = W.orderid
+JOIN stocks_in_bookstores AS V
+ON W.stockid = V.stockid
+JOIN books AS U
+ON V.pubid = U.pubid
+WHERE Q.HighestPub = W.item_price
